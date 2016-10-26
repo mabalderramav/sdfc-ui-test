9e3d7793826b65df94363cf7b769c72cfd3d1554
lourdesVillca <LourdesVillca@Fundacion-jala.local>
2016-10-26 16:01:10 -0400

    fix problem with tag

--- src/test/java/org/fundacionjala/sfdc/tests/lead/CreateLead.java
+++ src/test/java/org/fundacionjala/sfdc/tests/lead/CreateLead.java
@@ -3,18 +3,17 @@ package org.fundacionjala.sfdc.tests.lead;
 
 import java.util.Map;
 
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import org.fundacionjala.sfdc.framework.utils.JsonMapper;
 import org.fundacionjala.sfdc.pages.LoginPage;
 import org.fundacionjala.sfdc.pages.MainApp;
 import org.fundacionjala.sfdc.pages.TabBar;
 import org.fundacionjala.sfdc.pages.leads.LeadDetails;
 import org.fundacionjala.sfdc.pages.leads.LeadForm;
-
 import org.fundacionjala.sfdc.pages.leads.LeadHome;
-import org.fundacionjala.sfdc.framework.utils.JsonMapper;
 
 /**
  * This class verify the Creation of LeadHome.
@@ -55,7 +54,7 @@ public class CreateLead {
     /**
      * This method delete the created LeadFields.
      */
-    @AfterTest
+    @AfterMethod
     public void tearDown() {
        leadDetails.clickDeleteButton();
     }
