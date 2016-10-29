ba2bd8bbc258ee781583ac962c1e2cfb0c024060
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2016-10-29 09:23:03 -0400

    Itegration result

--- src/test/java/org/fundacionjala/sfdc/tests/lead/CreateLead.java
+++ src/test/java/org/fundacionjala/sfdc/tests/lead/CreateLead.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import org.fundacionjala.sfdc.framework.utils.JsonMapper;
